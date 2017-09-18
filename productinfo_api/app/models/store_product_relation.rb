f5652d7fda8ebecc0dd5d58b8b1c1ea7b178c85b
K RND <yonbanki@gmail.com>
2017-09-19 01:44:01 +0900

    add uniqueness validator to store_product_relation

--- productinfo_api/app/models/store_product_relation.rb
+++ productinfo_api/app/models/store_product_relation.rb
@@ -1,5 +1,6 @@
 class StoreProductRelation < ApplicationRecord
   validates :stock, presence:true
+  validates :product_id, uniqueness:{ scope:[:product_id,:store_id] }
   belongs_to :store
   belongs_to :product
 end
